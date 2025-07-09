887a9c1fb006ae076e641fddefcf5f1634d558ce
IT Internship <it-int@qattous.com>
2025-07-09 16:05:39 +0300

    update

---
+++ src/loginPage/MyTestCase.java
@@ -0,0 +1,38 @@
+package loginPage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyTestCase {
+	
+	
+	WebDriver driver= new ChromeDriver();
+	String theURL = "https://automationteststore.com/";
+	String SignupPage = "https://automationteststore.com/index.php?rt=account/create"; 
+
+	
+	
+	
+	@BeforeTest 
+	public void mysetup() {
+		driver.get(theURL);
+		driver.manage().window().maximize(); 
+		
+		
+	}
+	
+	
+	
+	@Test
+	public void Singup() {
+		
+		driver.navigate().to(SignupPage);
+		WebElement FirstName = driver.findElement(By.name("firstname"));
+		FirstName.sendKeys("omar");
+	}
+
+}
